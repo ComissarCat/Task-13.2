dd0b2af96a861a66be713c09fab6506b16aee166
Медведев Андрей Юрьевич <andrey.medvede@yandex.ru>
2024-01-19 11:25:38 +0300

    Добавьте файлы проекта.

---
+++ Task 13.2/Task 13.2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Squares
+{
+private:
+    vector<int> squares;
+public:
+    Squares()
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            squares.push_back(i * i);
+        }
+    }
+    friend ostream& operator<<(ostream& out, Squares squares)
+    {
+        for (vector<int>::iterator it = squares.squares.begin(); it != squares.squares.end(); it++)
+        {
+            out << *it << "\t";
+        }
+        return out;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Squares squares;
+    cout << squares;
+
+    return 0;
+}
